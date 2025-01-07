1ebec1d815f104aa2cb1def3a8d91559f129decf
shriyapalkar <132357599+shriyapalkar@users.noreply.github.com>
2025-01-07 22:06:08 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,42 @@
+from mysql.connector import MySQLConnection, Error
+from config import read_config
+
+def read_file(filename):
+    with open(filename, 'rb') as f:
+        photo =f.read()
+        return photo
+    
+def update_blob(author_id, filename):
+    #read file data
+    data=read_file(filename)
+    
+    #prepare update query and data
+    query='UPDATE authors' \
+                'SET photo = %s' \
+                'WHERE id =%s'
+                
+    args=(data,author_id) 
+    config=read_config()   
+try:
+    #establish a conn to mysql database
+    with MySQLConnection(**config) as conn:
+        #create a cursor to execute SQL queries
+        with conn.cursor() as cursor:
+            #execute the update query with the provided arguements
+            cursor.execute(query.args)
+            #commit the chnages to the database
+            conn.commit()
+            
+            
+except Error as e:
+    print(e)
+if __name__ == '__main__':
+
+    try:
+        author_id=3
+        filename='D:/Program Files/Blob in python/MySQL-Logo.jpg'
+        update_blob(author_id,filename)
+    
+    
+    except Error as e:
+        print(e)
